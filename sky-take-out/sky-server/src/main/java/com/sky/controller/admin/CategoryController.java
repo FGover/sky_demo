ac41f3593fcc225cf1d9e1b27b1b24e2a2e82ef1
fgh <2924074463@qq.com>
2024-12-21 18:28:45 +0800

    更新

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -6,6 +6,7 @@ import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/admin/category")
 @Slf4j
-@ApiOperation("分类相关接口")
+@Api(tags = "分类模块接口")
 public class CategoryController {
 
     @Autowired
