f79e54b7394aded18dbc3b8af5edef4ce2f3b960
fgh <2924074463@qq.com>
2024-12-21 17:40:30 +0800

    更新

---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishImpl.java
@@ -0,0 +1,6 @@
+package com.sky.service.impl;
+
+import com.sky.service.DishService;
+
+public class DishImpl implements DishService {
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetMealImpl.java
@@ -0,0 +1,6 @@
+package com.sky.service.impl;
+
+import com.sky.service.SetMealService;
+
+public class SetMealImpl implements SetMealService {
+}
